137e89cf745ece95ca3452b8703e3a6b5141e1fa
med-ghali <tagemouati.medghali@gmail.com>
2022-05-23 15:42:13 +0100

    bug fic

--- src/components/princingCard.vue
+++ src/components/princingCard.vue
@@ -2,10 +2,10 @@
   <div class="card p-0 m-2 " @click="focus" :class="{outsideBorder:active}">
         <div class="inner-div ">
         <div class="card-header text-center bg-primary p-3">
-            <h4  contenteditable="true" @dblclick="handleText" style="min-height: 90px; line-height: 90px;">Beginner</h4>
+            <h4  contenteditable="true" @dblclick="handleText" >Beginner</h4>
         </div>
         <div class="card-body text-center p-0">
-            <h1 class="bg-light mx-0 p-5" contenteditable="true" @dblclick="handleText">  $15 <small>/mo</small></h1>
+            <h1 class="bg-light mx-0 py-5" style="min-height: 75px; line-height: 75px;" contenteditable="true" @dblclick="handleText">  $15 <small>/mo</small></h1>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item" contenteditable="true" @dblclick="handleText">An item</li>
                 <li class="list-group-item" contenteditable="true" @dblclick="handleText">A second item</li>
